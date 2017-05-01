8acd304243abb8b2f36a395fc450579c91d682a4
James N. V. Cash <james.nvc@gmail.com>
2017-05-01 18:55:40 -0400

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jamesnvc/neovim-client "0.1.0"
+(defproject jamesnvc/neovim-client "0.1.1"
   :description "Client library for the Neovim RPC API"
   :url "https://github.com/clojure-vim/neovim-client"
   :license {:name "Eclipse Public License"
